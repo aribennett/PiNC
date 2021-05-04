02f77ca384a17aba96b33d6d2781c86d725f62a2
aridanielbennett <ari.bennett@hillman.group.com>
2021-05-03 20:26:45 -0700

    commented out circle

--- python-pinc/draw_ui.py
+++ python-pinc/draw_ui.py
@@ -28,7 +28,7 @@ def start_ui():
         # Draw a solid blue circle in the center
         circle_center = (250, HEIGHT+180)
         circle_radius = 240
-        pygame.draw.circle(screen, MAIN_COLOR, circle_center, circle_radius, width=3)
+        # pygame.draw.circle(screen, MAIN_COLOR, circle_center, circle_radius)
         for i in range(100):
             angle = (offset + i)*math.pi/50
             new_radius = circle_radius - 2
@@ -39,7 +39,7 @@ def start_ui():
             vec2 = (vec[0]*tic_length, vec[1]*tic_length)
             end = (circle_center[0] + vec[0], circle_center[1] + vec[1])
             end2 = (circle_center[0] + vec2[0], circle_center[1] + vec2[1])
-            pygame.draw.line(screen, MAIN_COLOR, end2, end, width=2)
+            pygame.draw.line(screen, MAIN_COLOR, end2, end)
 
         font = pygame.font.SysFont(None, 24)
         img = font.render(f'{offset}', True, MAIN_COLOR)
