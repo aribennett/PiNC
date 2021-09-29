b0757e42c6d789887e9a3160014990d80b8f74fa
Ari Bennett <aridbennett@gmail.com>
2021-09-29 11:11:54 -0700

    all motors working

--- software/test_serial.py
+++ software/test_serial.py
@@ -8,7 +8,7 @@ import os
 from xbox360controller import Xbox360Controller
 
 CONTROLLER_DEAD_ZONE = 0.2
-CONTROLLER_JOG_RATE = 50
+CONTROLLER_JOG_RATE = 100
 MAX_ACCELERATION = 1000
 
 x_nominal = 0
@@ -32,17 +32,17 @@ def handleSlider2(value):
 def embedded_service():
     packet_count = 0
     KP = 100
-    KD = 0
+    KD = 10
     KI = 0
     while True:
         hid_msg = read()
         header = pkt.unpack_HeaderPacket(hid_msg[:pkt.size_HeaderPacket])
         unpack_index = pkt.size_HeaderPacket
         for i in range(header.motorCount):
-            motor_packet = pkt.unpack_MotorPacket(
-                hid_msg[unpack_index:unpack_index+pkt.size_MotorPacket])
+            motor_packet = pkt.unpack_MotorStatePacket(
+                hid_msg[unpack_index:unpack_index+pkt.size_MotorStatePacket])
             embedded_motors[motor_packet.motorId] = motor_packet
-            unpack_index += pkt.size_MotorPacket
+            unpack_index += pkt.size_MotorStatePacket
         for i in range(header.sensorCount):
             sensor_packet = pkt.unpack_SensorPacket(
                 hid_msg[unpack_index:unpack_index+pkt.size_SensorPacket])
@@ -70,33 +70,37 @@ def embedded_service():
             x_nominal = 0
             y_nominal = 0
             z_nominal = 0
-        errorx = x_nominal + y_nominal - embedded_motors[0].omega
-        control_inputx = np.clip(
-            KP*errorx - KD*embedded_motors[0].omega, -MAX_ACCELERATION, MAX_ACCELERATION)
-        control_inputx = np.clip(
-            KP*errorx - KD*embedded_motors[0].omega, -MAX_ACCELERATION, MAX_ACCELERATION)
+        errorx = x_nominal + y_nominal - embedded_motors[3].omega
+        control_inputx = np.clip(KP*errorx - KD*embedded_motors[3].omega, -MAX_ACCELERATION, MAX_ACCELERATION)
 
-        errory = x_nominal - y_nominal - embedded_motors[1].omega
-        control_inputy = np.clip(
-            KP*errory - KD*embedded_motors[1].omega, -MAX_ACCELERATION, MAX_ACCELERATION)
-        errorz = z_nominal - embedded_motors[2].omega
+        errory = x_nominal - y_nominal - embedded_motors[4].omega
+        control_inputy = np.clip(KP*errory - KD*embedded_motors[4].omega, -MAX_ACCELERATION, MAX_ACCELERATION)
 
-        control_inputz = KP*errorz
+        errorz2 = z_nominal - embedded_motors[2].omega
+        control_inputz2 = KP*errorz2
+        errorz1 = z_nominal - embedded_motors[1].omega
+        control_inputz1 = KP*errorz1
+        errorz0 = z_nominal - embedded_motors[0].omega
+        control_inputz0 = KP*errorz0
 
         control_packet = pkt.pack_HeaderPacket(
-            pkt.SerialCommand.RUN_MOTOR, motorCount=3)
-        control_packet += pkt.pack_MotorPacket(
-            embedded_motors[0].motorId, pkt.MotorCommand.SET_ALPHA, alpha=control_inputx)
-        control_packet += pkt.pack_MotorPacket(
-            embedded_motors[1].motorId, pkt.MotorCommand.SET_ALPHA, alpha=control_inputy)
-        control_packet += pkt.pack_MotorPacket(
-            embedded_motors[2].motorId, pkt.MotorCommand.SET_ALPHA, alpha=control_inputz)
+            pkt.SerialCommand.RUN_MOTOR, motorCount=5)
+        control_packet += pkt.pack_MotorCommandPacket(
+            embedded_motors[3].motorId, pkt.MotorCommand.SET_ALPHA, control=control_inputx)
+        control_packet += pkt.pack_MotorCommandPacket(
+            embedded_motors[4].motorId, pkt.MotorCommand.SET_ALPHA, control=control_inputy)
+        control_packet += pkt.pack_MotorCommandPacket(
+            embedded_motors[2].motorId, pkt.MotorCommand.SET_ALPHA, control=control_inputz2)
+        control_packet += pkt.pack_MotorCommandPacket(
+            embedded_motors[1].motorId, pkt.MotorCommand.SET_ALPHA, control=control_inputz1)
+        control_packet += pkt.pack_MotorCommandPacket(
+            embedded_motors[0].motorId, pkt.MotorCommand.SET_ALPHA, control=control_inputz0)
         write(control_packet)
 
 
 if __name__ == "__main__":
     os.system(f"taskset -p -c 3 {os.getpid()}")
-    cold_start('/dev/hidraw0')
+    cold_start('/dev/hidraw5')
     with Xbox360Controller(0, axis_threshold=0.2) as controller:
         jog_controller = controller
         embedded_thread = Thread(target=embedded_service, daemon=True)
