9d8b18753cd252ae5c50e98506ae89842d30046d
aridanielbennett <ari.bennett@hillman.group.com>
2021-10-11 18:28:30 -0700

    added gcode solver

---
+++ software/gcode_test.py
@@ -0,0 +1,18 @@
+from gcodeparser import GcodeParser
+import numpy as np
+from matplotlib import pyplot as plt
+from time import sleep, time
+from gcode_solver import GcodeSolver
+# open gcode file and store contents as variable
+with open('box_gcode.gcode', 'r') as f:
+  gcode = f.read()
+
+path_planner = GcodeSolver(gcode)
+
+TIME = 100
+current_time = np.linspace(0, TIME, num=TIME*10000)
+position = path_planner.get_solution(current_time)
+
+plt.plot(position[:, 0], position[:, 1])
+plt.gca().set_aspect("equal")
+plt.show()
