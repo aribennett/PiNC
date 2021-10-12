5fc84044bb9ed6b92b1f12c28b28876cc9e68474
Ari Bennett <aridbennett@gmail.com>
2021-10-11 18:21:04 -0700

    added errors and shit

--- software/serial_host/serial_host/__init__.py
+++ software/serial_host/serial_host/__init__.py
@@ -1,34 +1,47 @@
+from glob import glob
 import os
+import logging
 
 is_windows = os.name == 'nt'
 if is_windows:
-  import hid
+    import hid
 
 pid = 0x0486
 vid = 0x16c0
 device = None
 
+error_count = 0
+MAX_SEND_ERRORS = 10
+
 
 def cold_start(device_name=None):
-  global device
-  if is_windows:
-    device = hid.Device(vid, pid)
-  else:
-    flags = os.O_RDWR
-    device = os.open(device_name, flags)
-  pass
+    global device
+    if is_windows:
+        device = hid.Device(vid, pid)
+    else:
+        flags = os.O_RDWR
+        device = os.open(device_name, flags)
+    pass
 
 
 def write(packet):
-  out = b'\00' + packet  # Teensy eats one byte
-  if is_windows:
-    device.write(out)
-  else:
-    os.write(device, out)
-
+    global error_count
+    try:
+        out = b'\00' + packet  # Teensy eats one byte
+        if is_windows:
+            device.write(out)
+        else:
+            os.write(device, out)
+        error_count = 0
+    except:
+        logging.error("Write Exception")
+        error_count += 1
+    
+    if error_count >= MAX_SEND_ERRORS:
+        raise Exception("Too many failed sends")
 
 def read():
-  if is_windows:
-    return device.read(64, 1000)
-  else:
-    return os.read(device, 64)
+    if is_windows:
+        return device.read(64, 1000)
+    else:
+        return os.read(device, 64)
--- software/test_serial.py
+++ software/test_serial.py
@@ -1,19 +1,28 @@
 import tkinter
+from numpy.lib.function_base import vectorize
 from serial_host import packet_definitions as pkt
 from serial_host import cold_start, read, write
 from threading import Thread
 import numpy as np
-from time import time
+import math
+from time import time, sleep
 import os
 from xbox360controller import Xbox360Controller
 
 CONTROLLER_DEAD_ZONE = 0.2
 CONTROLLER_JOG_RATE = 100
-MAX_ACCELERATION = 1000
+MAX_ACCELERATION = 100
+XY_MM_PER_RAD = 6.36619783227
 
 x_nominal = 0
 y_nominal = 0
 z_nominal = 0
+errorx = 0
+errory = 0
+v_errorx = 0
+v_errory = 0
+x_velocity_nominal = 0
+y_velocity_nominal = 0
 
 embedded_motors = {}
 embedded_sensors = {}
@@ -29,11 +38,15 @@ def handleSlider2(value):
     y_nominal = float(value)
 
 
+
 def embedded_service():
-    packet_count = 0
-    KP = 100
-    KD = 10
+    global errorx, errory, x_velocity_nominal, y_velocity_nominal, v_errorx, v_errory
+    KP = 10000
+    KP_VELOCITY = 1000
+    KD = 0
     KI = 0
+    count = 0
+    start_time = time()
     while True:
         hid_msg = read()
         header = pkt.unpack_HeaderPacket(hid_msg[:pkt.size_HeaderPacket])
@@ -50,38 +63,48 @@ def embedded_service():
             unpack_index += pkt.size_SensorPacket
 
         if jog_controller.button_trigger_l.is_pressed:
-            if jog_controller.axis_l.x > CONTROLLER_DEAD_ZONE:
-                x_nominal = (jog_controller.axis_l.x-CONTROLLER_DEAD_ZONE)*CONTROLLER_JOG_RATE
-            elif jog_controller.axis_l.x < -CONTROLLER_DEAD_ZONE:
-                x_nominal = (jog_controller.axis_l.x+CONTROLLER_DEAD_ZONE)*CONTROLLER_JOG_RATE
-            else:
-                x_nominal = 0
+            # if jog_controller.axis_l.x > CONTROLLER_DEAD_ZONE:
+            #     x_nominal = (jog_controller.axis_l.x-CONTROLLER_DEAD_ZONE)*CONTROLLER_JOG_RATE
+            # elif jog_controller.axis_l.x < -CONTROLLER_DEAD_ZONE:
+            #     x_nominal = (jog_controller.axis_l.x+CONTROLLER_DEAD_ZONE)*CONTROLLER_JOG_RATE
+            # else:
+            #     x_nominal = 0
 
-            if jog_controller.axis_l.y > CONTROLLER_DEAD_ZONE:
-                y_nominal = (-jog_controller.axis_l.y+CONTROLLER_DEAD_ZONE)*CONTROLLER_JOG_RATE
-            elif jog_controller.axis_l.y < -CONTROLLER_DEAD_ZONE:
-                y_nominal = (-jog_controller.axis_l.y-CONTROLLER_DEAD_ZONE)*CONTROLLER_JOG_RATE
-            else:
-                y_nominal = 0
+            # if jog_controller.axis_l.y > CONTROLLER_DEAD_ZONE:
+            #     y_nominal = (-jog_controller.axis_l.y+CONTROLLER_DEAD_ZONE)*CONTROLLER_JOG_RATE
+            # elif jog_controller.axis_l.y < -CONTROLLER_DEAD_ZONE:
+            #     y_nominal = (-jog_controller.axis_l.y-CONTROLLER_DEAD_ZONE)*CONTROLLER_JOG_RATE
+            # else:
+            #     y_nominal = 0
             
-            z_nominal = (jog_controller.trigger_l.value - jog_controller.trigger_r.value)*CONTROLLER_JOG_RATE
+            z_nominal = (jog_controller.trigger_l.value - jog_controller.trigger_r.value)*CONTROLLER_JOG_RATE/3
             
         else:
-            x_nominal = 0
-            y_nominal = 0
+            # x_nominal = 0
+            # y_nominal = 0
             z_nominal = 0
-        errorx = x_nominal + y_nominal - embedded_motors[4].omega
-        control_inputx = np.clip(KP*errorx - KD*embedded_motors[4].omega, -MAX_ACCELERATION, MAX_ACCELERATION)
+        RADIUS = 10
+        SPEED = .5
+        x_nominal = math.sin(SPEED*time())*RADIUS
+        y_nominal = math.cos(SPEED*time())*RADIUS
 
-        errory = x_nominal - y_nominal - embedded_motors[3].omega
-        control_inputy = np.clip(KP*errory - KD*embedded_motors[3].omega, -MAX_ACCELERATION, MAX_ACCELERATION)
+        x_velocity_nominal = SPEED*math.cos(SPEED*time())*RADIUS
+        y_velocity_nominal = -SPEED*math.sin(SPEED*time())*RADIUS
+        v_errorx = x_velocity_nominal + y_velocity_nominal - embedded_motors[4].omega
+        v_errory = x_velocity_nominal - y_velocity_nominal - embedded_motors[3].omega
+
+        errorx = x_nominal + y_nominal - embedded_motors[4].theta
+        control_inputx = np.clip(KP*errorx - KD*embedded_motors[4].omega + KP_VELOCITY*v_errorx, -MAX_ACCELERATION, MAX_ACCELERATION)
+
+        errory = x_nominal - y_nominal - embedded_motors[3].theta
+        control_inputy = np.clip(KP*errory - KD*embedded_motors[3].omega + KP_VELOCITY*v_errory, -MAX_ACCELERATION, MAX_ACCELERATION)
 
         errorz2 = z_nominal - embedded_motors[2].omega
-        control_inputz2 = KP*errorz2
+        control_inputz2 = 10*errorz2
         errorz1 = z_nominal - embedded_motors[1].omega
-        control_inputz1 = KP*errorz1
+        control_inputz1 = 10*errorz1
         errorz0 = z_nominal - embedded_motors[0].omega
-        control_inputz0 = KP*errorz0
+        control_inputz0 = 10*errorz0
 
         control_packet = pkt.pack_HeaderPacket(
             pkt.SerialCommand.RUN_MOTOR, motorCount=5)
@@ -100,17 +123,14 @@ def embedded_service():
 
 if __name__ == "__main__":
     os.system(f"taskset -p -c 3 {os.getpid()}")
-    cold_start('/dev/hidraw3')
+    cold_start('/dev/hidraw2')
     with Xbox360Controller(0, axis_threshold=0.2) as controller:
         jog_controller = controller
         embedded_thread = Thread(target=embedded_service, daemon=True)
         embedded_thread.start()
 
-        master = tkinter.Tk()
-        w = tkinter.Scale(master, from_=40, to=-40, command=handleSlider,
-                        variable=tkinter.DoubleVar(), width=40, length=200, resolution=.1)
-        w.pack(side=tkinter.LEFT)
-        w2 = tkinter.Scale(master, from_=40, to=-40, command=handleSlider2,
-                        variable=tkinter.DoubleVar(), width=40, length=200, resolution=.1)
-        w2.pack(side=tkinter.RIGHT)
-        tkinter.mainloop()
+        while True:
+            sleep(.1)
+            pos_error = math.sqrt(errorx**2 + errory**2)*XY_MM_PER_RAD
+            vel_error = math.sqrt(v_errorx**2 + v_errory**2)*XY_MM_PER_RAD
+            print(str(pos_error).ljust(30, ' '), v_errorx, v_errory)
