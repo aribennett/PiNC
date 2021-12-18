1f3f29de9dc8fec7ea39dab610a32ce287c8017b
Ari Bennett <aridbennett@gmail.com>
2021-12-18 11:49:35 -0800

    removed old libs

--- pinc-turret/src/main.cpp
+++ pinc-turret/src/main.cpp
@@ -1,9 +1,38 @@
-#include <Arduino.h>
+#include <StepperHFC.h>
+#include <TMCConfig.h>
+#include <PWMMotor.h>
+#include <RCServo.h>
+#include <SerialClient.h>
+#include <Motor.h>
 
-void setup() {
-  // put your setup code here, to run once:
+#define R_SENSE 0.11f // SilentStepStick series use 0.11
+
+TMC5160Stepper x_spi(34, R_SENSE);
+StepperHFC x_driver(2, 3, 10);
+TMC5160Stepper y_spi(33, R_SENSE);
+StepperHFC y_driver(4, 5, 40);
+
+PWMMotor flywheel(38);
+RCServo feeder(39);
+
+void setup()
+{
+    SPI.begin();
+    serialClient.coldStart();
+    coldStart2130(&y_spi, 8);
+    coldStart2130(&x_spi, 8);
+    x_driver.coldStart();
+    y_driver.coldStart();
+    flywheel.coldStart();
+    feeder.coldStart();
+    motorList.addMotor(&x_driver);
+    motorList.addMotor(&y_driver);
+    motorList.addMotor(&flywheel);
+    motorList.addMotor(&feeder);
+    startMotorTimer();
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
+void loop()
+{
+    serialClient.run();
 }
