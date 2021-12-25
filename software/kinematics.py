cfd038b310a1a31812ed88c60364d2c14d075e98
Ari Bennett <aridbennett@gmail.com>
2021-12-24 16:11:13 -0800

    Updated z2

--- software/kinematics.py
+++ software/kinematics.py
@@ -1,10 +1,12 @@
 z0 = (25.3, -1.26)
 z1 = (1.413, 50.016)
-z2 = (58, 50.016)
+z2 = (54, 50.016)
 center_home = (25, 25)
 
+
 def corexy_transform(x_nominal, y_nominal):
-    return -y_nominal - x_nominal ,y_nominal - x_nominal
+    return -y_nominal - x_nominal, y_nominal - x_nominal
+
 
 def corexy_inverse(m0, m1):
-    return  (-m0 - m1)/2, (m1 - m0)/2
+    return (-m0 - m1)/2, (m1 - m0)/2
