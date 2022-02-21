223dbaa007b2f95574b76d6bf19a06f33bc11644
Ari Bennett <aridbennett@gmail.com>
2022-02-20 16:34:50 -0800

    different logging

--- software/run_pinc.py
+++ software/run_pinc.py
@@ -407,4 +407,5 @@ if __name__ == "__main__":
         print("Started controls")
         while True:
             sleep(1)
-            print(main.get_motor_state(3), main.get_motor_state(4), state, jog_controller.button_a.is_pressed)
+            # print(main.get_motor_state(3)[0], main.get_motor_state(4)[], state, jog_controller.button_a.is_pressed)
+            print(main.get_motor_state(0)[0], HomeState.home_0)
