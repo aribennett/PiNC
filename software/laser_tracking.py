c927b005e7a2c6ef0c2a5af0948778d1118b49e9
Ari Bennett <aridbennett@gmail.com>
2022-04-02 19:32:34 -0700

    busted mask?

--- software/laser_tracking.py
+++ software/laser_tracking.py
@@ -25,8 +25,8 @@ def run_tracking_loop(debug=False):
         for raw in camera.capture_continuous(raw_capture, format='bgr', use_video_port=True):
             image = raw.array.copy()
             image = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
-            lower = np.array([120, 0, 0])
-            upper = np.array([200, 255, 255])
+            lower = np.array([0, 0, 0])
+            upper = np.array([255, 255, 255])
             mask = cv2.inRange(image, lower, upper)
             M = cv2.moments(mask)
             # calculate x,y coordinate of center
