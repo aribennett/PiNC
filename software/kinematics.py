0a3ceb9ec193d326bc2ff90ffd67231b724a9e1a
Ari Bennett <aridbennett@gmail.com>
2022-05-09 20:33:41 -0700

    fixed inversion

--- software/kinematics.py
+++ software/kinematics.py
@@ -9,4 +9,11 @@ def corexy_transform(x_nominal, y_nominal):
 
 
 def corexy_inverse(m0, m1):
-    return (-m0 - m1)/2, (-m1 + m0)/2
+    return (-m0 + m1)/2, (-m1 - m0)/2
+
+
+if __name__ == '__main__':
+    print(corexy_transform(*corexy_inverse(20, 45)))
+    print(corexy_transform(*corexy_inverse(-20, 45)))
+    print(corexy_transform(*corexy_inverse(20, -45)))
+    print(corexy_transform(*corexy_inverse(-20, -45)))
