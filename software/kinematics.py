653741c127ecd7ce22718d13bf5ac2edb98c2137
Ari Bennett <aridbennett@gmail.com>
2022-05-09 20:43:33 -0700

    ecentuaLL

--- software/kinematics.py
+++ software/kinematics.py
@@ -5,11 +5,11 @@ center_home = (-25, -25)
 
 
 def corexy_transform(x_nominal, y_nominal):
-    return - y_nominal + x_nominal, - y_nominal - x_nominal
+    return y_nominal - x_nominal, - y_nominal - x_nominal
 
 
 def corexy_inverse(m0, m1):
-    return (m0 - m1)/2, (-m1 - m0)/2
+    return (-m1 - m0)/2, (m0 - m1)/2
 
 
 if __name__ == '__main__':
