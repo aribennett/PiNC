bcf6ee6a5773720e58358be42a0d752b03f40f8c
Ari Bennett <aridbennett@gmail.com>
2022-06-05 18:53:50 -0700

    print out red

--- software/laser_tracking.py
+++ software/laser_tracking.py
@@ -48,7 +48,7 @@ def run_tracking_loop(debug=False):
             #     laser_x = 0
             if debug:
                 print(laser_x)
-                cv2.imwrite("laser.bmp", image)
+                cv2.imwrite("laser.bmp", red)
             raw_capture.truncate(0)
             if kill_loop:
                 break
