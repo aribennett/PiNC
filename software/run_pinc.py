a8414e7aa4c8b0ef284ab32e7d3abb847762aab8
Ari Bennett <aridbennett@gmail.com>
2022-06-05 19:38:47 -0700

    tune z

--- software/run_pinc.py
+++ software/run_pinc.py
@@ -15,7 +15,7 @@ from thermistor import get_thermistor_temp, get_ntc100k_temp
 XY_MM_PER_RAD = 6.36619783227
 Z_MM_PER_RAD = 0.63661977236
 E_MM_PER_RAD = .85
-FINE_Z = 32.5
+FINE_Z = 32.25
 
 # ------ Debug Variables --------
 errorx = 0
