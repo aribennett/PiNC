7917657ec803ee086e8748c7a9a2a6f5390433d7
Ari Bennett <aridbennett@gmail.com>
2022-07-17 16:06:48 -0700

    test full screen UI

--- software/test_ui.py
+++ software/test_ui.py
@@ -1,7 +1,11 @@
 import tkinter as tk
 
 window = tk.Tk()
+window.geometry("800x480")
 label = tk.Label(text="Python rocks!")
 label.pack()
-
+label = tk.Label(text="Socks!")
+label.pack()
+label = tk.Label(text="And Box!")
+label.pack()
 window.mainloop()
