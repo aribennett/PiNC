51919663f8586308d7c06fb460d1515e4d22349f
Ari Bennett <aridbennett@gmail.com>
2022-09-25 14:55:28 -0700

    whhyyyy

--- software/custom_pathing.py
+++ software/custom_pathing.py
@@ -1,19 +1,22 @@
 from path_planning.gcode_solver import GcodeSolver
 from matplotlib import pyplot as plt
 import numpy as np
-with open('gcode_examples/sphere.gcode', 'r') as f:
+with open('gcode_examples/box-test.gcode', 'r') as f:
     gcode = f.read()
 
 path_planner = GcodeSolver(gcode, start_position=[0, 0, 0])
 x = path_planner.posistion_array[:, 0]
 y = path_planner.posistion_array[:, 1]
 z = path_planner.posistion_array[:, 2]
+e = path_planner.posistion_array[:, 3]
 
+ve = path_planner.velocity_array[:, 3]
 xyz_normal = path_planner.posistion_array[1:] - path_planner.posistion_array[:-1]
 
 # print(np.linalg.norm(xyz_normal, axis=1))
 xyz_normal = xyz_normal / np.linalg.norm(xyz_normal, axis=1)[:, np. newaxis]
 
 print(xyz_normal)
-plt.plot(x, y)
+plt.plot(ve)
+# plt.plot(x, y)
 plt.show()
